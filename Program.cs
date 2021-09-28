d7bd803781ff74d6ce3cd30c7cdba7d93f3a0220
marian <marianziganov09@gmail.com>
2021-09-28 20:34:28 +0300

    my first program commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace progrme
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+           int sizeInt = sizeof(int);
+            
+            Console.WriteLine(" Size of int = " + sizeInt);
+            
+            double sizedoble = sizeof(double);
+            
+            Console.WriteLine("Size of double = " + sizedoble);
+
+            float sizefloat = sizeof(float);
+
+            Console.WriteLine("Size of float " + sizefloat);
+
+            short sizeshort = sizeof(short);
+
+            Console.WriteLine("Size of short = " + sizefloat);
+
+            Console.ReadKey();
+
+
+
+            
+
+            
+
+        }
+    }
+}
