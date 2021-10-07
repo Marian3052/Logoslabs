69cdb51b912298ae96dce5f2d007c4f2d17f5310
marian <marianziganov09@gmail.com>
2021-10-07 17:26:06 +0300

    FIX1

--- worker and people/Program.cs
+++ worker and people/Program.cs
@@ -2,11 +2,34 @@
 
 namespace worker_and_people
 {
+   public class People
+   {
+       public string name;
+       public void status()
+       {
+        Console.WriteLIne(name + "status:" + this.status);
+       }
+
+   }  
+   public class Wroker : People         
+    {
+     protected void status();
+     protected void position();
+     protected void function();
+    
+    }
+  
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Worker vova = new Worker();
+            vova.name = "Vova";
+
+            Guest roma = new Guest();
+            rome.name = "Roman";
+           Console.ReadKey();
         }
     }
 }
