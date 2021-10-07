c8f646e5edd2b4f81d715e19a3b480dbd690370b
marian <marianziganov09@gmail.com>
2021-10-07 14:31:41 +0300

    imitation ready

--- imitation/Program.cs
+++ imitation/Program.cs
@@ -2,11 +2,43 @@
 
 namespace imitation
 {
+    abstract class car{
+         
+public abstract void CompletSet ();
+
+     }
+
+    class Mustang : car{
+        public override void CompletSet()
+        {
+            Console.WriteLine("complete set:  GT");
+        }
+    }
+
+    
+
+    class BMW : car{
+
+        public override void CompletSet()
+        {
+            Console.WriteLine("complete set: M-sport");
+        }
+    }
+
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Mustang mustang = new Mustang();
+            BMW bmw = new BMW();
+            mustang.CompletSet();
+            bmw.CompletSet();
+
+            Console.ReadKey();
+
         }
     }
 }
