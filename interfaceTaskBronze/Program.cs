e5fd8fcce8a5be8cdbeb2d5298afd5ba54f5c19f
marian <marianziganov09@gmail.com>
2021-11-03 15:41:59 +0200

    inerfaceTaskBronze(ready)

--- interfaceTaskBronze/Program.cs
+++ interfaceTaskBronze/Program.cs
@@ -37,29 +37,70 @@ namespace interfaceTaskBronze
 
         public class Lion : ITalk, IEat, IDrink, ISleep
         {
-            
+
+            public string food;
+            public string _sleep;
+            public string _water;
             private int _energy { get; set; }
             public int energy => _energy;
             private int _thirst { get; set; }
             public int thirst => _thirst;
             private int _starvation { get; set; }
             public int starvation => _starvation;
+            public void talk()
+            {
+                System.Console.WriteLine("Raaaaar!!!!");
+            }
             public Lion()
             {
                 this._thirst = 100;
                 this._starvation = 100;
                 this._energy = 100;
             }
-            public void talk()
+
+            public void processFood()
             {
-                System.Console.WriteLine("Raaaaar!!!!");
+                if (this.food == "apple") this._starvation += 10;
+                else if (this.food != "apple") this._starvation -= 10;
+            }
+            public void eat(string food)
+            {
+                this.food = food;
+                this.processFood();
+                if (this._starvation <= 0) System.Console.WriteLine("You Died!!");
+            }
+            public void processSleep()
+            {
+                if (this._sleep == "sleep") this._energy += 12;
+                else if (this._sleep != "sleep") this._energy -= 5;
             }
 
+            public void sleep(string _sleep)
+            {
+                this._sleep = _sleep;
+                this.processSleep();
+
+            }
+            public void processDrink()
+            {
+                if (this._water == "coca-cola") this._thirst += 10;
+                else if (this._water != "coca-cola") this._thirst -= 10;
+            }
+            public void drink(string _water)
+            {
+                this._water = _water;
+                this.processDrink();
+            }
         }
         static void Main(string[] args)
         {
             Lion lion = new Lion();
+            Console.Write("Lion sey: ");
             lion.talk();
+            lion.eat("");
+            lion.drink("");
+            lion.sleep("sleep");
+
             (lion as IEat).starvaionStatus();
             (lion as IDrink).thirstStatus();
             (lion as ISleep).energyStatus();
