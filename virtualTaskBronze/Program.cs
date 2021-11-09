78a567c25e9d907fcf7c075d5ecdb85a780ee8ed
marian <marianziganov09@gmail.com>
2021-11-09 19:03:36 +0200

    virtual fix1

---
+++ virtualTaskBronze/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace virtualTaskBronze
+{
+   
+    class Program
+    {
+      
+     public  class Animal
+        {
+            public string name;
+            private int _step { get; set; }
+            public int step => _step = 0;
+
+            public Animal(string name, int _step)
+            {
+                this._step = _step;
+                this.name = name;
+            }
+
+
+            public virtual void getInfo()
+            {
+                System.Console.WriteLine($"{name} : {_step}");
+            }
+
+
+        }
+    
+        
+
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
