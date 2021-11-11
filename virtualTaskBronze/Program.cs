6fdcb95c7af74a2b4c6c32095f1095f8668eae1c
marian <marianziganov09@gmail.com>
2021-11-11 19:27:48 +0200

    virtual

--- virtualTaskBronze/Program.cs
+++ virtualTaskBronze/Program.cs
@@ -6,28 +6,50 @@ namespace virtualTaskBronze
     class Program
     {
 
-        public class Animal
+        public class Puma
         {
             public string name;
             private int _step { get; set; }
-            public int step => _step = 0;
+            public int step => _step  = 4;
 
-            public Animal(string name, int _step)
+
+            public Puma(string name = "Puma", int _step = 4) 
             {
-                this._step = _step;
+                this._step = step;
                 this.name = name;
             }
 
+            
 
             public virtual void getInfo()
             {
-                System.Console.WriteLine($"{name} : {_step}");
+                System.Console.WriteLine($"{name} : {step} meter");
             }
 
 
         }
-        class Puma : Animal
+        class LittlePuma : Puma
         {
+            public int independent { get; set; }
+
+            public LittlePuma(string name = "little Puma", int _step = 4, int independent = 5) : base(name, _step)
+            {
+                this.name = name;
+                
+                this.independent = independent;
+
+            }
+
+            public void getIndependent()
+            {
+                independent = 5;
+            }
+            public override void getInfo()
+            {
+                System.Console.WriteLine($"{name} :{step / 4} meter | time for independence {independent} days");
+            }
+
+
 
         }
 
@@ -35,7 +57,11 @@ namespace virtualTaskBronze
         static void Main(string[] args)
         {
 
+            Puma puma = new Puma();
+            puma.getInfo();
 
+            LittlePuma littlePuma = new LittlePuma();
+            littlePuma.getInfo();
 
             Console.ReadKey();
         }
